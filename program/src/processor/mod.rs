63336a01fab222fcd17ca76dd6932045d8f010e6
febo <febo@anza.xyz>
2024-06-17 16:53:56 +0100

    Remove require macro

--- program/src/processor/mod.rs
+++ program/src/processor/mod.rs
@@ -132,21 +132,3 @@ pub fn process_instruction<'a>(
         }
     }
 }
-
-#[macro_export]
-macro_rules! require {
-    ( $constraint:expr, $error:expr $(,)? ) => {
-        if !$constraint {
-            return Err($error.into());
-        }
-    };
-    ( $constraint:expr, $error:expr, $message:expr $(,)? ) => {
-        if !$constraint {
-            solana_program::msg!("Constraint failed: {}", $message);
-            return Err($error.into());
-        }
-    };
-    ( $constraint:expr, $error:expr, $message:literal, $($args:tt)+ ) => {
-        require!( $constraint, $error, format!($message, $($args)+) );
-    };
-}
