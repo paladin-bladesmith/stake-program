7395c5407832a8431d531cfefe598607df355f61
febo <febo@anza.xyz>
2024-06-26 13:29:11 +0100

    Add cargo fmt to lint

--- scripts/client/lint-rust.mjs
+++ scripts/client/lint-rust.mjs
@@ -4,4 +4,5 @@ import { getClippyToolchain, workingDirectory } from '../utils.mjs';
 
 // Check the client using clippy.
 cd(path.join(workingDirectory, 'clients', 'rust'));
+await $`cargo ${getClippyToolchain()} fmt --check ${process.argv.slice(3)}`;
 await $`cargo ${getClippyToolchain()} clippy ${process.argv.slice(3)}`;
--- scripts/program/lint.mjs
+++ scripts/program/lint.mjs
@@ -9,5 +9,6 @@ import {
 // Lint the programs using clippy.
 for (const folder of getProgramFolders()) {
   cd(`${path.join(workingDirectory, folder)}`);
+  await $`cargo ${getClippyToolchain()} fmt --check ${process.argv.slice(3)}`;
   await $`cargo ${getClippyToolchain()} clippy ${process.argv.slice(3)}`;
 }
