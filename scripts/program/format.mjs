f8e9749f06ec4c4fccc8f4b897167bbf8eb1193e
febo <febo@anza.xyz>
2024-06-28 22:55:47 +0100

    Fix toolchain name

--- scripts/program/format.mjs
+++ scripts/program/format.mjs
@@ -7,7 +7,7 @@ import {
   workingDirectory,
 } from '../utils.mjs';
 
-const toolchain = getToolchainArg(getClippyToolchain());
+const toolchain = getToolchainArg(getRustfmtToolchain());
 // Format the programs using rust fmt.
 for (const folder of getProgramFolders()) {
   cd(`${path.join(workingDirectory, folder)}`);
