afa65cb7962633bd2508b25164ac56e06389e96a
febo <febo@anza.xyz>
2024-07-03 13:19:49 +0100

    Add processor implementation

--- program/src/error.rs
+++ program/src/error.rs
@@ -52,6 +52,14 @@ pub enum StakeError {
     /// 10 - Authority is not set
     #[error("Authority is not set")]
     AuthorityNotSet,
+
+    /// 11 - Amount greater than stake amount
+    #[error("Amount greater than stake amount")]
+    InsufficientStakeAmount,
+
+    /// 12 - Amount should be greater than 0
+    #[error("Amount should be greater than 0")]
+    InvalidAmount,
 }
 
 impl PrintProgramError for StakeError {
--- program/src/processor/deactivate_stake.rs
+++ program/src/processor/deactivate_stake.rs
@@ -1,6 +1,16 @@
-use solana_program::{entrypoint::ProgramResult, pubkey::Pubkey};
+use std::num::NonZeroU64;
 
-use crate::instruction::accounts::{Context, DeactivateStakeAccounts};
+use solana_program::{
+    clock::Clock, entrypoint::ProgramResult, program_error::ProgramError, pubkey::Pubkey,
+    sysvar::Sysvar,
+};
+
+use crate::{
+    error::StakeError,
+    instruction::accounts::{Context, DeactivateStakeAccounts},
+    require,
+    state::Stake,
+};
 
 /// Deactivate staked tokens for the validator.
 ///
@@ -13,9 +23,60 @@ use crate::instruction::accounts::{Context, DeactivateStakeAccounts};
 ///
 /// Instruction data: amount of tokens to deactivate, as a little-endian u64
 pub fn process_deactivate_stake(
-    _program_id: &Pubkey,
-    _ctx: Context<DeactivateStakeAccounts>,
-    _amount: u64,
+    program_id: &Pubkey,
+    ctx: Context<DeactivateStakeAccounts>,
+    amount: u64,
 ) -> ProgramResult {
+    // Account valuidation.
+
+    // stake
+    // - owner must be the stake program
+    // - must be initialized
+
+    require!(
+        ctx.accounts.stake.owner == program_id,
+        ProgramError::InvalidAccountOwner,
+        "stake"
+    );
+
+    let data = &mut ctx.accounts.stake.try_borrow_mut_data()?;
+    let stake = bytemuck::try_from_bytes_mut::<Stake>(data)
+        .map_err(|_error| ProgramError::InvalidAccountData)?;
+
+    require!(
+        stake.is_initialized(),
+        ProgramError::UninitializedAccount,
+        "stake",
+    );
+
+    // authority
+    // - must be a signer
+    // - must match the authority on the stake account
+
+    require!(
+        ctx.accounts.stake_authority.is_signer,
+        ProgramError::MissingRequiredSignature,
+        "stake_authority"
+    );
+
+    require!(
+        ctx.accounts.stake_authority.key == &stake.authority,
+        StakeError::InvalidAuthority,
+        "stake_authority"
+    );
+
+    // Validate the amount.
+
+    require!(amount > 0, StakeError::InvalidAmount);
+
+    require!(stake.amount >= amount, StakeError::InsufficientStakeAmount);
+
+    // Deactivate the stake.
+    //
+    // If the stake is already deactivating, this will reset the deactivation.
+
+    stake.deactivating_amount = amount;
+    stake.deactivation_timestamp = NonZeroU64::new(Clock::get()?.unix_timestamp as u64);
+
     Ok(())
 }
