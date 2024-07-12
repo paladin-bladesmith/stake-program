fe3bbb1b2302b084cfccad7cf9a8bb197f23ed86
febo <febo@anza.xyz>
2024-07-12 14:41:59 +0100

    [wip]: Add processor implementation

--- program/src/processor/harvest_holder_rewards.rs
+++ program/src/processor/harvest_holder_rewards.rs
@@ -1,6 +1,13 @@
-use solana_program::{entrypoint::ProgramResult, pubkey::Pubkey};
+use paladin_rewards::accounts::HolderRewards;
+use solana_program::{entrypoint::ProgramResult, program_error::ProgramError, pubkey::Pubkey};
+use spl_token_2022::{extension::PodStateWithExtensions, pod::PodAccount};
 
-use crate::instruction::accounts::{Context, HarvestHolderRewardsAccounts};
+use crate::{
+    error::StakeError,
+    instruction::accounts::{Context, HarvestHolderRewardsAccounts},
+    require,
+    state::{calculate_eligible_rewards, create_vault_pda, find_stake_pda, Config, Stake},
+};
 
 /// Harvests holder SOL rewards earned by the given stake account.
 ///
@@ -24,8 +31,147 @@ use crate::instruction::accounts::{Context, HarvestHolderRewardsAccounts};
 /// 7. `[]` Stake token mint, to get total supply
 /// 8. `[]` SPL Token program
 pub fn process_harvest_holder_rewards(
-    _program_id: &Pubkey,
-    _ctx: Context<HarvestHolderRewardsAccounts>,
+    program_id: &Pubkey,
+    ctx: Context<HarvestHolderRewardsAccounts>,
 ) -> ProgramResult {
+    // Account validation.
+
+    // config
+    // - owner must be the stake program
+    // - must be initialized
+
+    require!(
+        ctx.accounts.config.owner == program_id,
+        ProgramError::InvalidAccountOwner,
+        "config"
+    );
+
+    let config_data = ctx.accounts.config.try_borrow_data()?;
+    let config = bytemuck::try_from_bytes::<Config>(&config_data)
+        .map_err(|_error| ProgramError::InvalidAccountData)?;
+
+    require!(
+        config.is_initialized(),
+        ProgramError::UninitializedAccount,
+        "config",
+    );
+
+    // stake
+    // - owner must be the stake program
+    // - must be initialized
+    // - derivation must match (validates the config account)
+
+    require!(
+        ctx.accounts.stake.owner == program_id,
+        ProgramError::InvalidAccountOwner,
+        "stake"
+    );
+
+    let mut stake_data = ctx.accounts.stake.try_borrow_mut_data()?;
+    let stake = bytemuck::try_from_bytes_mut::<Stake>(&mut stake_data)
+        .map_err(|_error| ProgramError::InvalidAccountData)?;
+
+    require!(
+        stake.is_initialized(),
+        ProgramError::UninitializedAccount,
+        "stake",
+    );
+
+    let (derivation, _) = find_stake_pda(&stake.validator, ctx.accounts.config.key, program_id);
+
+    require!(
+        ctx.accounts.stake.key == &derivation,
+        ProgramError::InvalidSeeds,
+        "stake",
+    );
+
+    // vault
+    // - must be the token account on the stake config account
+
+    require!(
+        ctx.accounts.vault.key == &config.vault,
+        StakeError::IncorrectVaultAccount,
+    );
+
+    let vault_data = ctx.accounts.vault.try_borrow_data()?;
+    // unpack to validate the mint
+    let vault = PodStateWithExtensions::<PodAccount>::unpack(&vault_data)?;
+
+    // mint
+    // - must match the stake vault mint
+
+    require!(
+        &vault.base.mint == ctx.accounts.mint.key,
+        StakeError::InvalidMint,
+        "mint"
+    );
+
+    // stake authority
+    // - must be a signer
+    // - must match the authority on the stake account
+
+    require!(
+        ctx.accounts.stake_authority.is_signer,
+        ProgramError::MissingRequiredSignature,
+        "stake authority",
+    );
+
+    require!(
+        ctx.accounts.stake_authority.key == &stake.authority,
+        StakeError::InvalidAuthority,
+        "stake authority",
+    );
+
+    // vault authority
+    // - derivation must match
+
+    let vault_signer = create_vault_pda(ctx.accounts.config.key, config.signer_bump, program_id)?;
+
+    require!(
+        ctx.accounts.vault_authority.key == &vault_signer,
+        StakeError::InvalidAuthority,
+        "vault authority",
+    );
+
+    // holder rewards (for the vault token account)
+    // - owner must be the rewards program
+    // - derivation must match
+
+    require!(
+        ctx.accounts.holder_rewards.owner == &paladin_rewards::ID,
+        ProgramError::InvalidAccountOwner,
+        "holder rewards",
+    );
+
+    let (derivation, _) = Pubkey::find_program_address(
+        &["holder".as_bytes(), ctx.accounts.vault.key.as_ref()],
+        &paladin_rewards::ID,
+    );
+
+    require!(
+        ctx.accounts.holder_rewards.key == &derivation,
+        ProgramError::InvalidSeeds,
+        "holder rewards",
+    );
+
+    // Determine the holder rewards.
+
+    let holder_rewards = HolderRewards::try_from(ctx.accounts.holder_rewards)?;
+    let rewards = calculate_eligible_rewards(
+        holder_rewards.last_accumulated_rewards_per_token,
+        stake.last_seen_holder_rewards_per_token(),
+        stake.amount,
+    )?;
+    // update the last seen holder rewards
+    stake.set_last_seen_holder_rewards_per_token(holder_rewards.last_accumulated_rewards_per_token);
+
+    // Withdraw the holder rewards to the destination account.
+    //
+    // Rewards are stored on the `vault` account.
+    //
+    // TODO: Need to withdraw the rewards to the destination account, but these are
+    // currently stored on the vault token account. We could use `WithdrawExcessLamports`
+    // and put back the excess lamports back into the vault token account.
+
     Ok(())
 }
--- program/src/state/mod.rs
+++ program/src/state/mod.rs
@@ -2,12 +2,29 @@ pub mod config;
 pub mod stake;
 
 pub use config::*;
-use solana_program::pubkey::Pubkey;
+use solana_program::{
+    program_error::ProgramError,
+    pubkey::{Pubkey, PubkeyError},
+};
 pub use stake::*;
+
+const REWARDS_PER_TOKEN_SCALING_FACTOR: u128 = 1_000_000_000; // 1e9
 
 #[inline(always)]
 pub fn find_vault_pda(config: &Pubkey, program_id: &Pubkey) -> (Pubkey, u8) {
     Pubkey::find_program_address(&["token-owner".as_bytes(), config.as_ref()], program_id)
+}
+
+#[inline(always)]
+pub fn create_vault_pda(
+    config: &Pubkey,
+    bump: u8,
+    program_id: &Pubkey,
+) -> Result<Pubkey, PubkeyError> {
+    Pubkey::create_program_address(
+        &["token-owner".as_bytes(), config.as_ref(), &[bump]],
+        program_id,
+    )
 }
 
 #[inline(always)]
@@ -34,4 +51,32 @@ pub fn get_stake_pda_signer_seeds<'a>(
         config.as_ref(),
         bump_seed,
     ]
+}
+
+/// Calculate the eligible rewards for a given token account balance.
+///
+/// This uses the same logic as the Rewards program.
+pub fn calculate_eligible_rewards(
+    current_accumulated_rewards_per_token: u128,
+    last_accumulated_rewards_per_token: u128,
+    token_account_balance: u64,
+) -> Result<u64, ProgramError> {
+    // Calculation:
+    //   (current_accumulated_rewards_per_token
+    //     - last_accumulated_rewards_per_token)
+    //   * token_account_balance
+    let marginal_rate = current_accumulated_rewards_per_token
+        .checked_sub(last_accumulated_rewards_per_token)
+        .ok_or(ProgramError::ArithmeticOverflow)?;
+
+    if marginal_rate == 0 {
+        Ok(0)
+    } else {
+        // Scaled by 1e9 to store 9 decimal places of precision.
+        marginal_rate
+            .checked_mul(token_account_balance as u128)
+            .and_then(|product| product.checked_div(REWARDS_PER_TOKEN_SCALING_FACTOR))
+            .and_then(|product| product.try_into().ok())
+            .ok_or(ProgramError::ArithmeticOverflow)
+    }
 }
