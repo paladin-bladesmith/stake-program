a7ae94dec0921df410429f8151419879726477d9
Joe Caulfield <joe.caulfield@anza.xyz>
2024-07-16 11:21:45 -0500

    lint clients

--- clients/rust/src/lib.rs
+++ clients/rust/src/lib.rs
@@ -2,6 +2,6 @@ mod generated;
 mod hooked;
 pub mod pdas;
 
-pub use generated::programs::STAKE_ID as ID;
+pub use generated::programs::PALADIN_STAKE_PROGRAM_ID as ID;
 pub use generated::*;
 pub use hooked::*;
