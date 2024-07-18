8b6bd4220c505f91d339b5f155efffb68d13ea17
febo <febo@anza.xyz>
2024-07-18 13:24:45 +0100

    Add more tests

--- clients/rust/tests/stake_tokens.rs
+++ clients/rust/tests/stake_tokens.rs
@@ -6,6 +6,7 @@ use paladin_stake_program_client::{
     accounts::{Config, Stake},
     errors::PaladinStakeProgramError,
     instructions::StakeTokensBuilder,
+    pdas::find_stake_pda,
 };
 use setup::{
     add_extra_account_metas_for_transfer,
@@ -13,10 +14,13 @@ use setup::{
     rewards::{create_holder_rewards, RewardsManager},
     stake::StakeManager,
     token::{create_token_account, mint_to, TOKEN_ACCOUNT_EXTENSIONS},
+    vote::create_vote_account,
 };
 use solana_program_test::{tokio, ProgramTest};
 use solana_sdk::{
+    account::AccountSharedData,
     instruction::InstructionError,
+    pubkey::Pubkey,
     signature::{Keypair, Signer},
     transaction::Transaction,
 };
@@ -381,7 +385,7 @@ async fn fail_stake_tokens_with_zero_amount() {
         .mint(config_manager.mint)
         .vault(config_manager.vault)
         .token_program(spl_token_2022::ID)
-        .amount(0) // <- stake 50 tokens
+        .amount(0) // <- 0 tokens
         .instruction();
 
     add_extra_account_metas_for_transfer(
@@ -411,4 +415,115 @@ async fn fail_stake_tokens_with_zero_amount() {
     // Then we expect an error.
 
     assert_custom_error!(err, PaladinStakeProgramError::InvalidAmount);
+}
+
+#[tokio::test]
+async fn fail_stake_tokens_with_uninitialized_stake_account() {
+    let mut program_test = ProgramTest::new(
+        "paladin_stake_program",
+        paladin_stake_program_client::ID,
+        None,
+    );
+    program_test.add_program(
+        "paladin_rewards_program",
+        paladin_rewards_program_client::ID,
+        None,
+    );
+    let mut context = program_test.start_with_context().await;
+
+    // Given a config.
+
+    let config_manager = ConfigManager::new(&mut context).await;
+
+    // And we initialize a holder rewards accounts and mint 100 tokens.
+
+    let rewards_manager = RewardsManager::new(
+        &mut context,
+        &config_manager.mint,
+        &config_manager.mint_authority,
+    )
+    .await;
+
+    mint_to(
+        &mut context,
+        &config_manager.mint,
+        &config_manager.mint_authority,
+        &rewards_manager.token_account,
+        100,
+        0,
+    )
+    .await
+    .unwrap();
+
+    // And we create the holder rewards account for the vault account.
+
+    create_holder_rewards(
+        &mut context,
+        &rewards_manager.pool,
+        &config_manager.mint,
+        &config_manager.vault,
+    )
+    .await;
+
+    // And an uninitialized stake account.
+
+    let validator_vote = create_vote_account(
+        &mut context,
+        &Pubkey::new_unique(),
+        &config_manager.authority.pubkey(),
+    )
+    .await;
+    let (stake_pda, _) = find_stake_pda(&validator_vote, &config_manager.config);
+
+    context.set_account(
+        &stake_pda,
+        &AccountSharedData::from(solana_sdk::account::Account {
+            lamports: 100_000_000,
+            data: vec![5; Stake::LEN],
+            owner: paladin_stake_program_client::ID,
+            ..Default::default()
+        }),
+    );
+
+    // When we try to stake the tokens to the uninitialized stake account.
+
+    let mut stake_ix = StakeTokensBuilder::new()
+        .config(config_manager.config)
+        .stake(stake_pda)
+        .source_token_account(rewards_manager.token_account)
+        .token_account_authority(rewards_manager.owner.pubkey())
+        .validator_vote(validator_vote)
+        .mint(config_manager.mint)
+        .vault(config_manager.vault)
+        .token_program(spl_token_2022::ID)
+        .amount(50)
+        .instruction();
+
+    add_extra_account_metas_for_transfer(
+        &mut context,
+        &mut stake_ix,
+        &paladin_rewards_program_client::ID,
+        &rewards_manager.token_account,
+        &config_manager.mint,
+        &config_manager.vault,
+        &rewards_manager.owner.pubkey(),
+        0,
+    )
+    .await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[stake_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &rewards_manager.owner],
+        context.last_blockhash,
+    );
+    let err = context
+        .banks_client
+        .process_transaction(tx)
+        .await
+        .unwrap_err();
+
+    // Then we expect an error.
+
+    assert_instruction_error!(err, InstructionError::UninitializedAccount);
 }
