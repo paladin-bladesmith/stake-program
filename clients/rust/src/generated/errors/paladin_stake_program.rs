b0899b1677f2adbe74659a052e0d8bcd3ea0c8a1
febo <febo@anza.xyz>
2024-07-18 10:23:59 +0100

    Rename stake program

--- clients/rust/src/generated/errors/paladin_stake_program.rs
+++ clients/rust/src/generated/errors/paladin_stake_program.rs
@@ -55,10 +55,7 @@ pub enum PaladinStakeProgramError {
     /// 14 - Active deactivation cooldown
     #[error("Active deactivation cooldown")]
     ActiveDeactivationCooldown = 0xE,
-    /// 15 - Incorrect vault account
-    #[error("Incorrect vault account")]
-    IncorrectVaultAccount = 0xF,
-    /// 16 - No deactivated tokens
+    /// 15 - No deactivated tokens
     #[error("No deactivated tokens")]
     NoDeactivatedTokens = 0xF,
     /// 16 - Incorrect vault account
