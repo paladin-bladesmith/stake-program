b46d034da51467534f6cf12ce3e4947cb8ff3554
febo <febo@anza.xyz>
2024-07-23 10:07:13 +0100

    Add mint and vault tests

--- clients/rust/tests/withdraw_inactive_stake.rs
+++ clients/rust/tests/withdraw_inactive_stake.rs
@@ -14,7 +14,9 @@ use setup::{
     config::ConfigManager,
     rewards::{create_holder_rewards, RewardsManager},
     stake::StakeManager,
-    token::{create_associated_token_account, mint_to},
+    token::{
+        create_associated_token_account, create_token_account, mint_to, TOKEN_ACCOUNT_EXTENSIONS,
+    },
 };
 use solana_program_test::{tokio, ProgramTest};
 use solana_sdk::{
@@ -827,4 +829,278 @@ async fn fail_withdraw_inactive_stake_with_wrong_config_account() {
     // Then we expect an error.
 
     assert_instruction_error!(err, InstructionError::InvalidSeeds);
+}
+
+#[tokio::test]
+async fn fail_withdraw_inactive_stake_with_wrong_mint() {
+    let mut program_test = ProgramTest::new(
+        "paladin_stake_program",
+        paladin_stake_program_client::ID,
+        None,
+    );
+    program_test.add_program(
+        "paladin_rewards_program",
+        paladin_rewards_program_client::ID,
+        None,
+    );
+    let mut context = program_test.start_with_context().await;
+
+    // Given a config and stake accounts.
+
+    let config_manager = ConfigManager::new(&mut context).await;
+    let stake_manager = StakeManager::new(&mut context, &config_manager.config).await;
+
+    // And we set total amount delegated = 100 on the config account.
+
+    let mut account = get_account!(context, config_manager.config);
+    let mut config_account = Config::from_bytes(account.data.as_ref()).unwrap();
+    config_account.token_amount_delegated = 100;
+    // "manually" update the config account data
+    account.data = config_account.try_to_vec().unwrap();
+    context.set_account(&config_manager.config, &account.into());
+
+    mint_to(
+        &mut context,
+        &config_manager.mint,
+        &config_manager.mint_authority,
+        &config_manager.vault,
+        100,
+        0,
+    )
+    .await
+    .unwrap();
+
+    // And we set the amount = 50 and inactive_account = 50 on the stake account.
+
+    let mut account = get_account!(context, stake_manager.stake);
+    let mut stake_account = Stake::from_bytes(account.data.as_ref()).unwrap();
+    // "manually" set the stake values
+    stake_account.amount = 50;
+    stake_account.inactive_amount = 50;
+    // "manually" update the stake account data
+    account.data = stake_account.try_to_vec().unwrap();
+    context.set_account(&stake_manager.stake, &account.into());
+
+    // And we create the holder rewards account for the vault and destination accounts.
+
+    let rewards_manager = RewardsManager::new(
+        &mut context,
+        &config_manager.mint,
+        &config_manager.mint_authority,
+    )
+    .await;
+
+    let owner = context.payer.pubkey();
+    let destination =
+        create_associated_token_account(&mut context, &owner, &config_manager.mint).await;
+
+    create_holder_rewards(
+        &mut context,
+        &rewards_manager.pool,
+        &config_manager.mint,
+        &destination,
+    )
+    .await;
+
+    // And we create a new config account so we have a different mint account.
+
+    let another_config_manager = ConfigManager::new(&mut context).await;
+
+    let another_rewards_manager = RewardsManager::new(
+        &mut context,
+        &another_config_manager.mint,
+        &another_config_manager.mint_authority,
+    )
+    .await;
+
+    create_holder_rewards(
+        &mut context,
+        &another_rewards_manager.pool,
+        &another_config_manager.mint,
+        &another_config_manager.vault,
+    )
+    .await;
+
+    // When we try to withdraw using the wrong mint account.
+
+    let (vault_authority, _) = find_vault_pda(&config_manager.config);
+
+    let mut withdraw_ix = WithdrawInactiveStakeBuilder::new()
+        .config(config_manager.config)
+        .stake(stake_manager.stake)
+        .vault(config_manager.vault)
+        .destination_token_account(destination)
+        .mint(another_config_manager.mint) // <- wrong mint
+        .stake_authority(stake_manager.authority.pubkey())
+        .vault_authority(vault_authority)
+        .token_program(spl_token_2022::ID)
+        .amount(50)
+        .instruction();
+
+    add_extra_account_metas_for_transfer(
+        &mut context,
+        &mut withdraw_ix,
+        &paladin_rewards_program_client::ID,
+        &config_manager.vault,
+        &another_config_manager.mint, // <- wrong mint
+        &destination,
+        &vault_authority,
+        50,
+    )
+    .await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[withdraw_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &stake_manager.authority],
+        context.last_blockhash,
+    );
+    let err = context
+        .banks_client
+        .process_transaction(tx)
+        .await
+        .unwrap_err();
+
+    // Then we expect an error.
+
+    assert_custom_error!(err, PaladinStakeProgramError::InvalidMint);
+}
+
+#[tokio::test]
+async fn fail_withdraw_inactive_stake_with_wrong_vault_account() {
+    let mut program_test = ProgramTest::new(
+        "paladin_stake_program",
+        paladin_stake_program_client::ID,
+        None,
+    );
+    program_test.add_program(
+        "paladin_rewards_program",
+        paladin_rewards_program_client::ID,
+        None,
+    );
+    let mut context = program_test.start_with_context().await;
+
+    // Given a config and stake accounts.
+
+    let config_manager = ConfigManager::new(&mut context).await;
+    let stake_manager = StakeManager::new(&mut context, &config_manager.config).await;
+
+    // And we set total amount delegated = 100 on the config account.
+
+    let mut account = get_account!(context, config_manager.config);
+    let mut config_account = Config::from_bytes(account.data.as_ref()).unwrap();
+    config_account.token_amount_delegated = 100;
+    // "manually" update the config account data
+    account.data = config_account.try_to_vec().unwrap();
+    context.set_account(&config_manager.config, &account.into());
+
+    mint_to(
+        &mut context,
+        &config_manager.mint,
+        &config_manager.mint_authority,
+        &config_manager.vault,
+        100,
+        0,
+    )
+    .await
+    .unwrap();
+
+    // And we set the amount = 50 and inactive_account = 50 on the stake account.
+
+    let mut account = get_account!(context, stake_manager.stake);
+    let mut stake_account = Stake::from_bytes(account.data.as_ref()).unwrap();
+    // "manually" set the stake values
+    stake_account.amount = 50;
+    stake_account.inactive_amount = 50;
+    // "manually" update the stake account data
+    account.data = stake_account.try_to_vec().unwrap();
+    context.set_account(&stake_manager.stake, &account.into());
+
+    // And we create the holder rewards account for the vault and destination accounts.
+
+    let rewards_manager = RewardsManager::new(
+        &mut context,
+        &config_manager.mint,
+        &config_manager.mint_authority,
+    )
+    .await;
+
+    let owner = context.payer.pubkey();
+    let destination =
+        create_associated_token_account(&mut context, &owner, &config_manager.mint).await;
+
+    create_holder_rewards(
+        &mut context,
+        &rewards_manager.pool,
+        &config_manager.mint,
+        &destination,
+    )
+    .await;
+
+    // And we create a new config account so we have a different mint account.
+
+    // And we create a fake vault token account.
+
+    let wrong_vault = Keypair::new();
+    create_token_account(
+        &mut context,
+        &config_manager.authority.pubkey(),
+        &wrong_vault,
+        &config_manager.mint,
+        TOKEN_ACCOUNT_EXTENSIONS,
+    )
+    .await
+    .unwrap();
+
+    create_holder_rewards(
+        &mut context,
+        &rewards_manager.pool,
+        &config_manager.mint,
+        &wrong_vault.pubkey(),
+    )
+    .await;
+
+    // When we try to withdraw using the wrong mint account.
+
+    let (vault_authority, _) = find_vault_pda(&config_manager.config);
+
+    let mut withdraw_ix = WithdrawInactiveStakeBuilder::new()
+        .config(config_manager.config)
+        .stake(stake_manager.stake)
+        .vault(wrong_vault.pubkey()) // <- wrong vault
+        .destination_token_account(destination)
+        .mint(config_manager.mint)
+        .stake_authority(stake_manager.authority.pubkey())
+        .vault_authority(vault_authority)
+        .token_program(spl_token_2022::ID)
+        .amount(50)
+        .instruction();
+
+    add_extra_account_metas_for_transfer(
+        &mut context,
+        &mut withdraw_ix,
+        &paladin_rewards_program_client::ID,
+        &wrong_vault.pubkey(), // <- wrong vault
+        &config_manager.mint,
+        &destination,
+        &vault_authority,
+        50,
+    )
+    .await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[withdraw_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &stake_manager.authority],
+        context.last_blockhash,
+    );
+    let err = context
+        .banks_client
+        .process_transaction(tx)
+        .await
+        .unwrap_err();
+
+    // Then we expect an error.
+
+    assert_custom_error!(err, PaladinStakeProgramError::IncorrectVaultAccount);
 }
